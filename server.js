cedc1baff570be77672b4fd1622b2d8517da0dc7
BoyCoDon <thenq@dtp-education.com>
2024-08-12 14:50:28 +0700

    chore: Update server.js to fix response message

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('Hello World 2222!');
+  res.end('Hello World 33333!');
 }).listen(8888);
